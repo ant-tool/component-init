04f7e2c304149842fa903cf7125055aee695fd06
silentcloud <silentcloud@silentcloudMBP.local>
2016-05-11 11:03:35 +0800

    add test coverage & add test

--- boilerplate/tests/index-test.js
+++ boilerplate/tests/index-test.js
@@ -1,3 +1,5 @@
+import ComponentName from '../src/component';
+
 describe('global variables', function() {
   it('should all exist' , function() {
     expect(sinon).to.not.be.null;
@@ -5,5 +7,23 @@ describe('global variables', function() {
     expect(should).to.not.be.null;
     expect(1).to.not.be.true;
     [1, 2].should.have.length(2);
+  });
+});
+
+describe('ComponentName func', function() {
+  let consoleSpy;
+  beforeEach(function() {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+  afterEach(function() {
+    console.log.restore();
+  });
+  it('should console A', function() {
+    ComponentName.pigcanA();
+    expect(consoleSpy).to.be.calledWith('A');
+  });
+  it('should console B', function() {
+    ComponentName.pigcanB();
+    expect(consoleSpy).to.be.calledWith('B');
   });
 });
