ecd4439506489e7f2abad7990aebfb59604ef469
silentcloud <silentcloud@silentcloudMBP.local>
2016-05-11 10:06:25 +0800

    use atool-test-mocha

--- boilerplate/tests/index-test.js
+++ boilerplate/tests/index-test.js
@@ -1,8 +1,9 @@
-describe('first test', function() {
-  it('should be eql' , function() {
-    expect(1).to.not.be.true;
+describe('global variables', function() {
+  it('should all exist' , function() {
     expect(sinon).to.not.be.null;
     expect(expect).to.not.be.null;
     expect(should).to.not.be.null;
+    expect(1).to.not.be.true;
+    [1, 2].should.have.length(2);
   });
 });
